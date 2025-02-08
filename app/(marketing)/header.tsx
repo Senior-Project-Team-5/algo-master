5f4bbe10d69c8b9df04fe71acdcb832dba429387
Junhui Huang <95193970+NightFall28@users.noreply.github.com>
2025-02-08 16:31:39 -0500

    Initial Setup, Clerk Auth
    - Clerk Authentication
    - Initial Project Set Up
    - Footer
    - Header
    - Layout Formatting
    - Buttons Library

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+export const Header = () => {
+    return ( 
+        <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    {/* TODO: add image logo */}
+                    {/* <Image src="/some.svg" height={40} width={40} alt="Logo"/> */}
+                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+                        <a href="/">
+                            AlgoMaster
+                        </a>
+                    </h1>
+                </div>
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton mode="modal" fallbackRedirectUrl={"/buttons"}>
+                            <Button size={"lg"} variant={"ghost"}>
+                                Sign In
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
+        </header>
+     );
+}
