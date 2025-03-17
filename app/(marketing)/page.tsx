3b16303a9e356e1e670911f5d93f45c0fafdcc73
dyzhao1011 <mikey501zhao@gmail.com>
2025-03-16 21:20:53 -0400

    Updated Marketing Page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,38 +1,66 @@
 import { Button } from "@/components/ui/button";
-import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
-
+import Image from "next/image";
+import "./page.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-green-500 font-bold">
-        Marketing Page
-      </h1>
-      <ClerkLoading>
-        <p className="text-green-400">Loading...</p>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <SignedOut>
-          <SignUpButton mode="modal" fallbackRedirectUrl={"/home"}>
-            <Button variant={"secondary"} size={"lg"} className="w-full">
-              Get Started
-            </Button>
-          </SignUpButton>
-          <SignInButton mode="modal" fallbackRedirectUrl={"/home"}>
-            <Button variant={"secondaryOutline"} size={"lg"} className="w-full">
-              Already have an account? Sign in here
-            </Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <Button variant={"primary"} size={"lg"} className="w-full" asChild>
-            <Link href="/home">
-              Continue Learning
-            </Link>
-          </Button>
-        </SignedIn>
-      </ClerkLoaded>
+    <div id="home">
+      <div id="home-left">
+        <div id="home-left-info">
+          <Image src="/logo.svg" height={500} width={500} alt="Logo" />
+          <h1 id="home-left-info-title">
+            Test Your Data Structures & Algorithms Knowledge With Us
+          </h1>
+          <h1 id="home-left-info-subtitle">Join Us Now</h1>
+        </div>
+        <div id="home-left-clerk">
+          <ClerkLoading>
+            <p className="text-green-400">Loading...</p>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" fallbackRedirectUrl={"/roadmap"}>
+                <Button variant={"secondary"} size={"lg"}>
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" fallbackRedirectUrl={"/roadmap"}>
+                <Button variant={"secondaryOutline"} size={"lg"}>
+                  Already have an account? Sign in here
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button
+                variant={"primary"}
+                size={"lg"}
+                className="w-full"
+                asChild
+              >
+                <Link href="/roadmap">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+
+      <div id="home-image">
+        <Image
+          src="/coding picture.svg"
+          alt="coding image"
+          fill
+          objectFit="cover"
+        />
+      </div>
     </div>
-  )
+  );
 }
