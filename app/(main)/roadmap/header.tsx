c840cbed4d4396534fc3d6c82e352d954baf1bdc
Tenzin Choezom <123757698+cherry-code@users.noreply.github.com>
2025-04-01 18:09:39 -0400

    Update header.tsx

--- app/(main)/roadmap/header.tsx
+++ app/(main)/roadmap/header.tsx
@@ -8,12 +8,12 @@ type Props = {
 
 export const Header = ({ title }: Props) => {
     return (
-        <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 mb-5 text-neutral-400 lg:z-50">
-            <Link href="/home">
+        <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] lg:mt-[-28px] flex justify-center items-center text-center border-b-2 mb-5 mx-auto text-neutral-400 lg:z-50 w-full">
+            {/*<Link href="/home">
                 <Button>
                     <ArrowLeft className="h-5 w-5 stroke-2 text-neutral-400" />
                 </Button>
-            </Link>
+            </Link>*/}
             <h1 className="font-bold text-lg">
                 {title}
             </h1>
