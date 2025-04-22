29c50b2a7f7ef9135840bf341b56fd0bc01ab345
Junhui Huang <95193970+NightFall28@users.noreply.github.com>
2025-04-22 18:09:54 -0400

    Update Prequiz.tsx

--- app/(main)/roadmap/[topic]/components/Prequiz/Prequiz.tsx
+++ app/(main)/roadmap/[topic]/components/Prequiz/Prequiz.tsx
@@ -6,24 +6,11 @@ const Prequiz: React.FC<Topic> = ({ topic }) => {
   return (
     <div>
       <h3>{decodeURIComponent(topic)}</h3>
-      {/* <h1>
-        <br />
-        Questions includes
-      </h1> */}
-      {/* Currently, the following prompts are hard coded, but later, it will be retrieved from the database */}
-      {/* <ul>
-        <li>Fundamentals of arrays</li>
-        <li>Identifying an array function</li>
-        <li>Identifying the result of an array function</li>
-      </ul> */}
 
       <p>
         <br />
         <br />
         The quiz will end once you have reached 10 points. Each correct answer gives you +1 point, while incorrect answers result in -1 point. Your score will always be between 0 and 10.
-      </p>
-      <p>
-        You can end the quiz anytime, but you will lose your progress.
       </p>
       <p>
         <br />
